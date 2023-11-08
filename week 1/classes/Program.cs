585f00be2cfb79cc8d66bb1b1d7bf44c34cbd5ca
JosueSdev <josue.sandoval@bigcommerce.com>
2023-11-08 13:57:57 -0600

    classes

---
+++ week 1/classes/Program.cs
@@ -0,0 +1,9 @@
+﻿using Classes;
+
+var account = new BankAccount("Josué Sandoval", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+
+Console.WriteLine(account.GetAccountHistory());
